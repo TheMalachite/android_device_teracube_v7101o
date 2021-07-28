PixelExperience-Devices/device_teracube_v7101o

v7101o: Initial device tree

AndroidProducts.mk
@@ -1,0 +1,8 @@
+#
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_v7101o.mk

TheMalachite/android_device_teracube_v7101o

Change-Id: I75423b5c1f6e63c1cac413d4522fae587b5b11c4

AndroidProducts.mk
@@ -1,0 +1,8 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_v7101o.mk